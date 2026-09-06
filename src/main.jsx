0187aaf4678b0fc924490022cb7f4f4df310e177
RuthJala <thecleanlines27@gmail.com>
2026-09-06 23:48:51 +0530

    Load premium website refresh

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App.jsx";
 import "./styles.css";
+import "./premium-refresh.css";
+import "./premium-refresh.js";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
