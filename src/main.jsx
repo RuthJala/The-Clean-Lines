909130f5c6d8cf2ee122a1a4e0533ecb85c3850e
RuthJala <thecleanlines27@gmail.com>
2026-09-07 00:59:46 +0530

    Load final transparent logo sizing

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { App } from "./App.jsx";
 import "./styles.css";
 import "./premium-refresh.css";
+import "./logo-fix.css";
 import "./premium-refresh.js";
 
 createRoot(document.getElementById("root")).render(
