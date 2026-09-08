6c77d7ab72b7e8791fedf1d859600efd7f4aa15b
RuthJala <thecleanlines27@gmail.com>
2026-09-08 15:37:38 +0530

    Enable final swipe interactions

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,9 @@ import "./styles.css";
 import "./premium-refresh.css";
 import "./logo-fix.css";
 import "./luxury-final.css";
+import "./final-interactions.css";
 import "./premium-refresh.js";
+import "./final-interactions.js";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
